652d188ef249b00e025e109da7553c27a573f1ad
Godwin254 <godwinbee15@gmail.com>
2020-06-10 11:26:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import PostApp from './PostApp';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
