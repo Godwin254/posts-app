a22753196b5025bb54a5fa8b1580aac3f6bf7bac
Godwin254 <godwinbee15@gmail.com>
2020-06-10 11:39:53 +0300

    made request for post items from jsonplaceholder API

--- PostApp.js
+++ PostApp.js
@@ -3,7 +3,22 @@ import React, {useEffect, useState} from 'react';
 function PostApp(){
 
   const [post, setPost] = useState({});
+  const [error, setError] = useState('')
   
+
+  useEffect(()=>{
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${}`)
+      .then(res => {
+        console.log(res);
+        setPost(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+        setError(error = 'failed to fetch post');
+      })
+  })
+
   return (
     <div>
     
